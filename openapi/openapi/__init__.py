08f6fa35cc414c7f056d71936b522c8936acbb9c
gwvsol <gwvsol@gmail.com>
2021-12-23 18:45:01 +0300

    добавлен __init__

---
+++ openapi/openapi/__init__.py
@@ -0,0 +1,21 @@
+"""
+OpenAPI stream to Websoket
+"""
+
+__author__ = 'Fedorov Mikhail'
+__maintainer__ = 'Fedorov Mikhail'
+
+__email__ = 'jwvsol@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+__doc__ = 'OpenAPI stream to Websoket'
+
+
+__all__ = (
+    __author__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+    __doc__,
+)
